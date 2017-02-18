65dcace52cb573f770ff0156436817478ee3e457
tmoher <moher@toms-laptop.local>
2017-02-18 09:29:05 -0600

    nutella problem or mongodb problem?

--- bots/roomcast/node-bot.js
+++ bots/roomcast/node-bot.js
@@ -7,7 +7,7 @@ var componentId = NUTELLA.parseComponentId();
 var nutella = NUTELLA.init(cliArgs.broker, cliArgs.app_id, cliArgs.run_id, componentId);
 // Optionally you can set the resource Id
 nutella.setResourceId('my_resource_id');
-
+console.log('got started');
 
 var portals = nutella.persist.getMongoObjectStore('portals');
 portals.load(function(){ 
