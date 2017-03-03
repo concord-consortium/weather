b255bfc60d9d47dfbecdb9d6463352950218cd13
Noah Paessel <knowuh@gmail.com>
2017-03-03 10:57:28 -0500

    Use an image file for Nutella
    TBD:  Will have to move this to the Concord Dockerhub repo eventually.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 cd /app
+mkdir ./log
 /usr/bin/mongod --fork --logpath ./log/mongod.log
+echo "mongo started"
+sleep 5
 /usr/local/bundle/bin/nutella start
+echo "nutella started"
+
 # keep the container running
 tail -f ./log/mongod.log
